f5e4189ef28f8cca88ed353cee6884e721ed1c07
ShotaTogawa <s.togawa0112@gmail.com>
2020-02-16 22:00:39 -0800

    made env with docker

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mysqlUser: "root",
-  mysqlHost: "localhost",
-  mysqlDatabase: "myapp",
-  mysqlPassword: "",
-  mysqlPort: 3306
-};
--- config/prod.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mysqlUser: process.env.PGUSER,
-  mysqlHost: process.env.MYSQL_HOST,
-  mysqlDatabase: process.env.MYSQL_DATABASE,
-  mysqlPassword: process.env.MYSQL_PASSWORD,
-  mysqlPort: process.env.MYSQL_PORT
-};
