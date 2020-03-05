59c8536e49bddb8521419dc5267727f27fb57267
ShotaTogawa <s.togawa0112@gmail.com>
2020-03-05 10:03:36 -0800

    made user router

--- src/router/user.js
+++ src/router/user.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const { authenticateToken } = require('../controllers/auth')
-const { updateProfile } = require('../controllers/user')
+const express = require("express");
+const router = express.Router();
+const { authenticateToken } = require("../controllers/auth");
+const { updateProfile, getCurrentUser } = require("../controllers/user");
 
-router.patch('/user/edit', authenticateToken, updateProfile)
+router.get("/user/:id", authenticateToken, getCurrentUser);
+// router.get("/user", authenticateToken, getUsers);
+router.patch("/user/edit", authenticateToken, updateProfile);
+// router.delete("/user/:id", authenticateToken, deleteUser);
 
-module.exports = router
+module.exports = router;
