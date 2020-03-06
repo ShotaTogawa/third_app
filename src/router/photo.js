0406ef3503b6578d983ac1ed563465a4091c8d35
ShotaTogawa <s.togawa0112@gmail.com>
2020-03-05 23:13:31 -0800

    fixed format

--- src/router/photo.js
+++ src/router/photo.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const router = express.Router();
-const { authenticateToken } = require("../controllers/auth");
+const express = require('express')
+const router = express.Router()
+const { authenticateToken } = require('../controllers/auth')
 const {
   photo,
   myPhotos,
@@ -10,15 +10,15 @@ const {
   createPhoto,
   updatePhoto,
   deletePhoto
-} = require("../controllers/photo");
+} = require('../controllers/photo')
 
-router.get("/photo/:photoId", authenticateToken, photo);
-router.get("/my-photos", authenticateToken, myPhotos);
-router.get("/my-photos/search", authenticateToken, searchMyPhotos);
-router.get("/photos", authenticateToken, photos);
-router.get("/photos/search", authenticateToken, searchPhotos);
-router.post("/photo", authenticateToken, createPhoto);
-router.put("/photo/:photoId", authenticateToken, updatePhoto);
-router.delete("/photo/:photoId", authenticateToken, deletePhoto);
+router.get('/photo/:photoId', authenticateToken, photo)
+router.get('/my-photos', authenticateToken, myPhotos)
+router.get('/my-photos/search', authenticateToken, searchMyPhotos)
+router.get('/photos', authenticateToken, photos)
+router.get('/photos/search', authenticateToken, searchPhotos)
+router.post('/photo', authenticateToken, createPhoto)
+router.put('/photo/:photoId', authenticateToken, updatePhoto)
+router.delete('/photo/:photoId', authenticateToken, deletePhoto)
 
-module.exports = router;
+module.exports = router
