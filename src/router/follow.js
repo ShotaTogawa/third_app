b5f1143fee0ee669452b77ec85cf8e6a03951b5b
ShotaTogawa <s.togawa0112@gmail.com>
2020-03-12 19:45:53 -0700

    made follow router

---
+++ src/router/follow.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { authenticateToken } = require('../controllers/auth');
+const { follow, unfollow } = require('../controllers/follow');
+
+router.post('/follow/:userId', authenticateToken, follow);
+router.delete('/unfollow/:userId', authenticateToken, unfollow);
