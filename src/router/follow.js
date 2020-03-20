13e4032ae7329c27a24681008f5e5ed504627ae8
ShotaTogawa <s.togawa0112@gmail.com>
2020-03-19 21:08:57 -0700

    added routers and controllers for followee and followers data

--- src/router/follow.js
+++ src/router/follow.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { authenticateToken } = require('../controllers/auth');
-const { follow, unfollow } = require('../controllers/follow');
+const {
+  follow,
+  unfollow,
+  follower,
+  followee
+} = require('../controllers/follow');
 
 router.post('/follow/:userId', authenticateToken, follow);
 router.delete('/unfollow/:userId', authenticateToken, unfollow);
+router.get('/follower', authenticateToken, follower);
+router.get('/followee', authenticateToken, followee);
 
 module.exports = router;
