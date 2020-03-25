409be1064d4934dac4207804cdb8e88e1d75215b
ShotaTogawa <s.togawa0112@gmail.com>
2020-03-26 08:43:13 +0900

    fixed database.js and added env variables to variables.env

--- db/database.js
+++ db/database.js
@@ -1,31 +1,72 @@
 const Sequelize = require('sequelize');
+require('dotenv').config({ path: 'variables.env' });
+const {
+  NODE_ENV,
+  DEV_DB_USERNAME,
+  DEV_DB_NAME,
+  DEV_DB_HOST,
+  DEV_DB_PASSWORD,
+  DEV_DB_DIARECT,
+  TEST_DB_USERNAME,
+  TEST_DB_NAME,
+  TEST_DB_HOST,
+  TEST_DB_PASSWORD,
+  TEST_DB_DIARECT,
+  PROD_DB_USERNAME,
+  PROD_DB_NAME,
+  PROD_DB_HOST,
+  PROD_DB_PASSWORD,
+  PROD_DB_DIARECT
+} = process.env;
 
-if (process.env.NODE_ENV === 'test') {
-  module.exports = new Sequelize('circle_test', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+if (NODE_ENV === 'test') {
+  module.exports = new Sequelize(
+    TEST_DB_NAME,
+    TEST_DB_USERNAME,
+    TEST_DB_PASSWORD,
+    {
+      host: TEST_DB_HOST,
+      dialect: TEST_DB_DIARECT,
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
     }
-  });
-} else if (process.env.NODE_ENV === 'production') {
-  // production info
+  );
+} else if (NODE_ENV === 'production') {
+  module.exports = new Sequelize(
+    PROD_DB_NAME,
+    PROD_DB_USERNAME,
+    PROD_DB_PASSWORD,
+    {
+      host: PROD_DB_HOST,
+      dialect: PROD_DB_DIARECT,
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
+    }
+  );
 } else {
-  module.exports = new Sequelize('myapp', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+  module.exports = new Sequelize(
+    DEV_DB_NAME,
+    DEV_DB_USERNAME,
+    DEV_DB_PASSWORD,
+    {
+      host: DEV_DB_HOST,
+      dialect: DEV_DB_DIARECT,
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
     }
-  });
+  );
 }
 
 // module.exports = db;
